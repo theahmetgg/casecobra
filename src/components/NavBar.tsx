f0b3e39def87533c7f27526e9800e3a777bde058
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-31 22:33:02 +0300

    Add signup and login functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,16 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const NavBar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
-    <nav className="sticky z-[100] h-14 inset-x-o top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
@@ -29,7 +32,7 @@ const NavBar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -38,12 +41,21 @@ const NavBar = () => {
                     Dashboard ✨
                   </Link>
                 ) : null}
-               
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
               </>
             ) : (
               <>
                 <Link
-                  href="/api/auth/logout"
+                  href="/api/auth/register"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
@@ -61,7 +73,9 @@ const NavBar = () => {
                 >
                   Login
                 </Link>
-                <div className="h-8 w-px bg-zinc-200 hidden sm:block"  />
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
                 <Link
                   href="/configure/upload"
                   className={buttonVariants({
@@ -81,4 +95,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default Navbar;
