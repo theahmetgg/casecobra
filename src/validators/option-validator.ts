5340aa65ec467c6d54b43a0af203beeb83ac4162
theahmetgg <ahmet1998gulmez@gmail.com>
2024-08-10 17:29:33 +0300

    add color selection for background in DesignConfigurator.tsx, allows changing case background color

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,19 @@
+//bg-zinc-900 border-zinc-900
+//bg-blue-950 border-blue-950
+//bg-rose-950 border-rose-950
+//bg-green-950 border-green-950
+
+
+export const COLORS=[
+    {
+        label:" Black",value:"black",tw:"zinc-900"
+    },{
+        label:" Blue",value:"blue",tw:"blue-950"
+    },
+    {
+        label:" Rose",value:"rose",tw:"rose-950"
+    },
+    {
+        label:" Green",value:"green",tw:"green-950"
+    }
+] as const
